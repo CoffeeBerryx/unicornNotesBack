37574d579b1c0847d4cd18661d233c4a28a950ea
Emma Alvarez Gonzalez <personal@ealvarez.info>
2024-10-28 23:11:34 -0700

    simplified sqlsession

--- app/main.py
+++ app/main.py
@@ -10,13 +10,3 @@ app.include_router(user_routes.router)
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
--- app/user/user_controller.py
+++ app/user/user_controller.py
@@ -2,19 +2,18 @@ from sqlmodel import select
 
 from app.user.user_models import User, UserCreate
 from app.utils.sqlite_db import get_session
+sqlSession = get_session()
 
 def get_all_users_controller():
-    session = get_session()
-    users = session.exec(select(User)).all()
+    users = sqlSession.exec(select(User)).all()
     return users
 
 def get_user_controller():
     return {"method": "get user"}
 
 def create_user_controller(user: UserCreate):
-    session = get_session() #necesito que me pases la conexion a la base de datos
     db_user = User(name=user.name, lastName=user.lastName, age=user.age)
-    session.add(db_user) #agrego el usuario
-    session.commit() #hago el commit (haz la chamba)
-    session.refresh(db_user) #actualizo el usuario con el nuevo id
+    sqlSession.add(db_user) #agrego el usuario
+    sqlSession.commit() #hago el commit (haz la chamba)
+    sqlSession.refresh(db_user) #actualizo el usuario con el nuevo id
     return db_user
